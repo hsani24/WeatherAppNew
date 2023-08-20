13e70167efd6f8c29b158476b56893663aa1bba2
hsani24 <hsani24@gmail.com>
2023-08-20 19:00:36 +0200

    Initial Commit

---
+++ WeatherAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppNew
+//
+//  Created by Sani I on 20.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
